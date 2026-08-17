282d184c42a47534d987ee9e9d09d9f6c6038f17
nekowawolf <irukainu20@gmail.com>
2026-08-17 17:44:41 +0700

    refactor: replace next/image with native img tag

--- src/components/FallbackImage.tsx
+++ src/components/FallbackImage.tsx
@@ -1,19 +1,17 @@
 'use client';
 
 import React, { useState } from 'react';
-import Image, { ImageProps } from 'next/image';
 
 export const FALLBACK_IMAGE_URL = 'https://nekowawolf.github.io/cdn-images/images/2026/1780148714_image-unavailable.png';
 
-export const FallbackImage = ({ src, alt, ...props }: ImageProps) => {
+export const FallbackImage = ({ src, alt, ...props }: React.ImgHTMLAttributes<HTMLImageElement>) => {
     const [imgSrc, setImgSrc] = useState(src);
 
     return (
-        <Image
+        <img
             {...props}
-            src={imgSrc ? imgSrc : FALLBACK_IMAGE_URL}
+            src={imgSrc ? (imgSrc as string) : FALLBACK_IMAGE_URL}
             alt={alt || 'Image'}
-            unoptimized={true}
             onError={() => {
                 if (imgSrc !== FALLBACK_IMAGE_URL) {
                     setImgSrc(FALLBACK_IMAGE_URL);
